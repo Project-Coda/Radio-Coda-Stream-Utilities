2c7f71ec70239d29dc8c7a11776db2ba64fa7852
projectcodaofficial <git@projectcoda.studio>
2022-06-26 01:45:21 +0000

    fix drawtext

--- rootfs/ffmpeg.sh
+++ rootfs/ffmpeg.sh
@@ -5,10 +5,10 @@ ldconfig && ffmpeg \
     -thread_queue_size 1024 -i "$AUDIO_SOURCE" \
     -map 0:v:0 -map 1:a:0 \
     -map_metadata:g 1:g \
+    -filter:v fps=$FPS \
     -vf drawtext="fontsize=50: fontfile=$FONT: \
         box=0: boxcolor=black@0.5: boxborderw=20: \
         textfile=$NP_SOURCE: reload=1: fontcolor=white@0.8: x=50: y=th" \
-    -filter:v fps=$FPS \
     -c:v h264_nvenc -g $(($FPS * 2)) \
     -b:v $VBR \
     -f flv "$YOUTUBE_URL/$KEY"
